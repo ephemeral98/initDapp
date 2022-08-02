214633079f45cb6be5039957c29db422c3a983c3
ephemeral98 <3502969036@qq.com>
2022-08-02 16:25:45 +0800

    修改路由类型约束

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,10 @@
-import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router';
+import {
+  createRouter,
+  createWebHashHistory,
+  RouteRecordRaw,
+  RouteLocationNormalized,
+  NavigationGuardNext,
+} from 'vue-router';
 import { useAppStore } from '@/store/appStore';
 import $load from '@cps/GlobalLoading';
 import { checkRightChain } from './routerHelp';
@@ -31,23 +37,25 @@ const router = createRouter({
   routes,
 });
 
-router.beforeEach(async (to, from, next) => {
-  // 每次进来，先确定是否拿到钱包
-  const appStore = useAppStore();
-  if (appStore.defaultAccount || !to.meta.requireAccount) {
-    // 进入的时候不依赖钱包拿链上数据的，先放用户进去，再不阻塞地获取钱包
-    appStore.linkWallet();
-  } else {
-    $load({ isShow: true });
-    await appStore.linkWallet();
-    $load({ isShow: false });
+router.beforeEach(
+  async (to: RouteLocationNormalized, from: RouteLocationNormalized, next: NavigationGuardNext) => {
+    // 每次进来，先确定是否拿到钱包
+    const appStore = useAppStore();
+    if (appStore.defaultAccount || !to.meta.requireAccount) {
+      // 进入的时候不依赖钱包拿链上数据的，先放用户进去，再不阻塞地获取钱包
+      appStore.linkWallet();
+    } else {
+      $load({ isShow: true });
+      await appStore.linkWallet();
+      $load({ isShow: false });
+    }
+    next(true);
+    try {
+      checkRightChain(to, from);
+    } catch (error) {
+      console.log('error..', error);
+    }
   }
-  next(true);
-  try {
-    checkRightChain(to, from);
-  } catch (error) {
-    console.log('error..', error);
-  }
-});
+);
 
 export default router;
